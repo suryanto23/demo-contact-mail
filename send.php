e90772126e195c6b748867dac2316a8625621a6a
suryanto23 <sur23.id@gmail.com>
2021-05-30 22:14:19 +0700

    forgot to add to

--- send.php
+++ send.php
@@ -3,6 +3,7 @@ $nama = $_POST['name'];
 $email = $_POST['email'];
 $messages = $_POST['messages'];
 $subject = "test email";
+$to = "sur23.id@gmail.com"
     
 $headers .= 'from : blablabla@mail.com'; //bagian ini diganti sesuai dengan email dari pengirim
 
